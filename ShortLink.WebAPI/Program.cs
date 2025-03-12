e29c4114bd8614da4b301668e8c2842632a5914c
mdlka <mogilevskiy499@gmail.com>
2025-03-12 19:14:34 +0300

    Initial commit

---
+++ ShortLink.WebAPI/Program.cs
@@ -0,0 +1,15 @@
+namespace ShortLink.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
