9408e9da1dce7bf673e919fe894f4aec2f153d9e
mdlka <mogilevskiy499@gmail.com>
2025-03-13 20:13:12 +0300

    Setup minimal api

---
+++ ShortLink.Core/Extensions/ServicesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShortLink.Core.Persistence;
+using ShortLink.Core.Services;
+
+namespace ShortLink.Core.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void AddCoreServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("short_link"));
+            services.AddTransient<IShortLinkService, ShortLinkService>();
+        }
+    }
+}
--- ShortLink.WebAPI/Program.cs
+++ ShortLink.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using ShortLink.Core.Extensions;
+using ShortLink.Core.Services;
+
 namespace ShortLink.WebAPI
 {
     public class Program
@@ -5,10 +8,18 @@ namespace ShortLink.WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddCoreServices();
+            
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
-
+            app.UseHttpsRedirection();
+            
+            app.MapPost("/", (string url, IShortLinkService shortLinkService) 
+                => shortLinkService.CreateShortUrl(url));
+            
+            app.MapGet("/{code}", (string code, IShortLinkService shortLinkService) 
+                => shortLinkService.GetOriginalUrl(code));
+            
             app.Run();
         }
     }
