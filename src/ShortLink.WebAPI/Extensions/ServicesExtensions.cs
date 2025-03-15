a6c513219ad0d30aabfa726472bf9c5bcb95ff9a
mdlka <mogilevskiy499@gmail.com>
2025-03-15 22:03:23 +0300

    Add CoreExceptionFilter

--- src/ShortLink.WebAPI/Extensions/ServicesExtensions.cs
+++ src/ShortLink.WebAPI/Extensions/ServicesExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace ShortLink.WebAPI.Extensions
+﻿using ShortLink.WebAPI.Filters;
+
+namespace ShortLink.WebAPI.Extensions
 {
     public static class ServicesExtensions
     {
         public static void AddWebApiServices(this IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options => options.Filters.Add<CoreExceptionFilterAttribute>());
             services.AddSwaggerGen();
         }
     }
---
+++ src/ShortLink.WebAPI/Filters/CoreExceptionFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ShortLink.Core.Exceptions;
+
+namespace ShortLink.WebAPI.Filters
+{
+    public class CoreExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        private readonly Dictionary<Type, Func<IActionResult>> _resultFactories = new()
+        {
+            { typeof(NotFoundException), () => new NotFoundResult() },
+        };
+        
+        public override void OnException(ExceptionContext context)
+        {
+            if (!_resultFactories.TryGetValue(context.Exception.GetType(), out var resultFactory)) 
+                return;
+            
+            context.Result = resultFactory.Invoke();
+            context.ExceptionHandled = true;
+        }
+    }
+}
